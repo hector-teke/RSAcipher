35e9b4f1ec633a866286f05bfc0c37052b850308
hector_teke <hectorguzman422@uma.es>
2023-11-28 13:46:22 +0100

    Text_to_number function is finished

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 import math
 import random
 
+###################### OT -> Integer
+
 def text_to_number(OT):
-    return OT
+    num = ""
+    while len(OT) >= 5:
+        num = str(block_to_int(OT[-5:])) + num
+        #print(OT[-5:], " \tnum: ", num)
+        OT = OT[0:-5]
+
+    if len(OT) > 0:
+        num = str(block_to_int(OT)) + num
+        #print(OT, "  \tnum: ", num)
+
+    return num
 
 def block_to_int(blk):
     bin_string = ""
@@ -18,7 +30,10 @@ def int_to_bin(n):
     bin_string = bin(n)[2:]
     return (8 - len(bin_string)) * '0' + bin_string
 
+###################### Integer -> OT
+
+def number_to_text(num):
+    bin_ = #112 key
 
 if __name__ == '__main__':
-    print(block_to_int("AA"))
-
+    print(text_to_number("I WANNA TAKE YOU AWAY"))
